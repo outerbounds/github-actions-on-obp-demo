254e444eaeeb254c6d5b93befc3a4dc86695a060
Christian Boylston <65568739+christianboylston@users.noreply.github.com>
2023-08-01 12:42:55 -0400

    more garbage

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 EVALUATE_DEPLOYMENT_CANDIDATES_COMMAND = ["python", "evaluate_deployment_candidates.py"]
 
 # This is the threshold that determines whether a model is a candidate for deployment.
-# In practice, you might define this by comparing the result against a baseline model's performance.
+# In practice, you might define this by comparing the esult against a baseline model's performance.
 PERFORMANCE_THRESHOLDS = {
     'accuracy': 90
 }
